61d5848bc8261a61b026159fb49c8b7ff5fbe40f
Diana Zaharova <Dysua1999@mail.ru>
2020-12-05 12:00:55 +0300

    Добавила React.Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,29 +5,32 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 import 'fontsource-roboto';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Купить продукты',
-      isDone: false
-    },
-    {
-      value: 'Заправить машину',
-      isDone: false
-    },
-    {
-      value: 'Постирать кроссовки',
-      isDone: true
-    }
-  ];
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Купить продукты',
+        isDone: false
+      },
+      {
+        value: 'Заправить машину',
+        isDone: false
+      },
+      {
+        value: 'Постирать кроссовки',
+        isDone: true
+      }
+    ];
 
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Todos</h1>
-      <InputItem />
-      <ItemList items={items} />
-      <Footer count={3} />
-    </div>);
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Todos</h1>
+        <InputItem />
+        <ItemList items={items} />
+        <Footer count={3} />
+      </div>);
+
+  }
 }
 
 export default App;
