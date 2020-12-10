e41cd8e6c6878403f8dd8a41f7ddab60875734af
Diana Zaharova <Dysua1999@mail.ru>
2020-12-10 17:23:49 +0300

    Исправление ошибки в pull request

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import styles from './InputItem.module.css';
 import Button from '@material-ui/core/Button';
-import PropTypes from 'prop-types'
 
 class InputItem extends React.Component {
   state = {
