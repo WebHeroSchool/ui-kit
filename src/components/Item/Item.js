c87e0be5fdcdfcee740db29b1312494a62f6c315
Diana Zaharova <Dysua1999@mail.ru>
2020-12-11 15:29:15 +0300

    Методы жизненного цикла

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,23 +5,39 @@ import 'fontsource-roboto';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const Item = ({ value, isDone, onClickDone, id, onClickDelete }) => (
-  <div className={styles.wrap}>
-    <Checkbox
-      checked={isDone}
-      onClick={() => onClickDone(id)} />
-    <span className={
-      classnames({
-        [styles.item]: true,
-        [styles.done]: isDone
-      })}> {value}
-    </span >
-    <div
-      className={styles.delete_icon}
-    ><DeleteIcon
-        onClick={() => onClickDelete(id)} />
-    </div>
-  </div>
-);
+class Item extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+  };
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  };
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  };
+
+  render() {
+    const { value, isDone, onClickDone, id, onClickDelete } = this.props;
+    return (
+      <div div className={styles.wrap} >
+        <Checkbox
+          checked={isDone}
+          onClick={() => onClickDone(id)}
+        />
+        <span className={
+          classnames({
+            [styles.item]: true,
+            [styles.done]: isDone
+          })}> {value}
+        </span >
+        <div className={styles.delete_icon}>
+          <DeleteIcon onClick={() => onClickDelete(id)} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Item;
