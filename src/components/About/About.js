1bfe808cbed9b907eab31cf0d5814377c8f7c0be
Diana Zaharova <Dysua1999@mail.ru>
2020-12-18 18:46:52 +0300

    Fetch

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Octokit from '@octokit/rest';
 
-const About = () => (
-  <CardContent>About</CardContent>
-);
+const octokit = new Octokit();
+
+class About extends React.Component {
+  state = {
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    octokit.repos.listForUser({
+      username: 'DianaZaharova'
+    }).then(({ data }) => console.log(data));
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <CardContent>
+        <h1>{isLoading ? <CircularProgress color="secondary" /> : 'About'}</h1>
+      </CardContent>
+    );
+  }
+}
 
 export default About;
