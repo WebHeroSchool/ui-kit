e2cb5a62a286e16b3b4c3defd820b2cc64cbd169
Diana <60700254+DianaZaharova@users.noreply.github.com>
2020-12-27 16:08:42 +0300

    удалила UseEffect из import

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
